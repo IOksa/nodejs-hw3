a6f079b05dac30112ff606e30ccc0044f780b3a0
IOksa <117656767+IOksa@users.noreply.github.com>
2023-09-04 19:40:35 +0300

    Initial commit

---
+++ routes/api/contacts.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ctrl = require("../../controllers/contacts");
+
+const {validateBody, isValidId} = require("../../middlewares");
+const {schemas} = require("../../models/contact");
+
+const router = express.Router();
+
+router.get('/', ctrl.getAll);
+
+router.get('/:id', isValidId, ctrl.getById);
+
+router.delete('/:id', isValidId, ctrl.deleteById);
+
+router.post('/', validateBody(schemas.addSchema), ctrl.add);
+
+router.put('/:id', isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+
+router.patch('/:id/favorite', isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateStatusContact);
+
+module.exports = router;
